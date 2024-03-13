b5f77fabd1df879e35f54b797ce154f2395a5dbf
Adina Weinberg <adina.weinberg@teladochealth.com>
2024-03-13 16:20:38 -0400

    Make successful fetch call to AEM

--- i18n.js
+++ i18n.js
@@ -1,4 +1,4 @@
-import { I18n } from 'i18n-js';
+import { I18n } from '@td/td-i18n-js';
 
 const i18nCMS = new I18n({
     app: 'any_app',
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { i18nCMS } from "./i18n.js";
 
-console.log(i18nCMS);
+console.log("i18ncms object: " + i18nCMS);
 // console.log(i18nCMS.
-console.log(i18nCMS.t('mhd.basics.errors_labels.height'));
+console.log(i18nCMS.t('mhd'));
